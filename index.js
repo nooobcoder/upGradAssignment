e3391c8c0fd9c54858a479566d03ade1496aac30
nooobcoder <suryashi2013@gmail.com>
2021-06-21 17:51:27 +0000

    Part One (A) Complete

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var signUpModal = document.getElementById("signUpModal");
+var signInModal = document.getElementById("signInModal");
+var composePostModal = document.getElementById("composePostModal");
 
 const showSignUpModal = () => {
 	signUpModal.style.display = "block";
 };
 
-const span = document.getElementsByClassName("close")[0];
-span.onclick = function () {
-	signUpModal.style.display = "none";
+const showSignInModal = () => {
+	signInModal.style.display = "block";
 };
+const showComposePostModal = () => {
+	composePostModal.style.display = "block";
+};
+
+const raiseSignUp = () => {
+	signInModal.style.display = "none";
+	showSignUpModal();
+};
+
+const span = document.getElementsByClassName("close");
+for (const close of span) {
+	close.onclick = function () {
+		signUpModal.style.display = "none";
+		signInModal.style.display = "none";
+		composePostModal.style.display = "none";
+	};
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == signUpModal) {
+		signUpModal.style.display = "none";
+	} else if (event.target == signInModal) {
+		signInModal.style.display = "none";
+	} else if (event.target == composePostModal) {
+		composePostModal.style.display = "none";
+	}
+};
+
+signUpModal.style.display = "none";
+signInModal.style.display = "none";
+composePostModal.style.display = "none";
