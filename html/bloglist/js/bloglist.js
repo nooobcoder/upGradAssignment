abcbd0e1a37e2694adbb78e867f086757d18e396
nooobcoder <suryashi2013@gmail.com>
2021-06-21 20:47:49 +0000

    Fixed Modal Logic

--- html/bloglist/js/bloglist.js
+++ html/bloglist/js/bloglist.js
@@ -1,4 +1,25 @@
-var deletePostModal = document.getElementById("composePostModal");
+var deletePostModal = document.getElementById("deletePostModal");
+var signUpModal = document.getElementById("signUpModal");
+var signInModal = document.getElementById("signInModal");
+const showSignUpModal = () => {
+	signUpModal.style.display = "block";
+};
+
+const showSignInModal = () => {
+	signInModal.style.display = "block";
+};
+const raiseSignUp = () => {
+	signInModal.style.display = "none";
+	showSignUpModal();
+};
+const span = document.getElementsByClassName("close");
+for (const close of span) {
+	close.onclick = function () {
+		signUpModal.style.display = "none";
+		signInModal.style.display = "none";
+		deletePostModal.style.display = "none";
+	};
+}
 
 const showDeletePostModal = () => {
 	deletePostModal.style.display = "block";
@@ -10,6 +31,13 @@ const hideDeletePostModal = () => {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
 	if (event.target == deletePostModal) deletePostModal.style.display = "none";
+	else if (event.target == signUpModal) {
+		signUpModal.style.display = "none";
+	} else if (event.target == signInModal) {
+		signInModal.style.display = "none";
+	}
 };
 
+signUpModal.style.display = "none";
+signInModal.style.display = "none";
 deletePostModal.style.display = "none";
