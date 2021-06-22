004d98076d16ecaad0b691240d58bf31a45e551c
nooobcoder <suryashi2013@gmail.com>
2021-06-22 05:31:48 +0000

    Project Complete - Optimisation needed

--- html/posts/js/post.js
+++ html/posts/js/post.js
@@ -43,12 +43,62 @@ window.onclick = function (event) {
 signUpModal.style.display = "none";
 signInModal.style.display = "none";
 
-const likesCount = 0;
+// post_heading editing_post_heading
+// post_body editing_post_body
+var postHeaderDiv = document.getElementById("post_heading");
+var postContentDiv = document.getElementById("post_body");
+var editing = false;
+var editButton = document.getElementById("btn_edit_post");
+
+const editDocument = () => {
+	editing = !editing;
+	if (editing) {
+		postHeaderDiv.setAttribute("class", "editing_post_heading");
+		postHeaderDiv.contentEditable = true;
+
+		postContentDiv.contentEditable = true;
+		postContentDiv.setAttribute("class", "editing_post_body");
+		const saveIcon = document.createElement("i");
+		saveIcon.setAttribute("class", "fas fa-save");
+		editButton.innerText = "Save ";
+		editButton.append(saveIcon);
+	} else {
+		const editIcon = document.createElement("i");
+		editIcon.setAttribute("class", "fas fa-edit");
+		editButton.innerText = "Edit ";
+		editButton.append(editIcon);
+
+		postHeaderDiv.removeAttribute("class");
+		postHeaderDiv.contentEditable = false;
+
+		postContentDiv.removeAttribute("class");
+		postContentDiv.contentEditable = false;
+	}
+};
+editButton.addEventListener("click", editDocument);
+
+var likesCount = 0;
 const describeLikes = () => {
 	const likeTag = document.getElementById("like_counter");
-	likesCount === 0
-		? (likeTag.innerText = "Be the first one to like this!")
-		: "";
+	likeTag.innerText =
+		likesCount === 0
+			? "Be the first one to like this!"
+			: likesCount === 1
+			? "1 person likes this!"
+			: `${likesCount} people have liked this!`;
 	likesCount += 1;
 };
 describeLikes();
+
+const addComment = () => {
+	const commentTextArea = document.getElementById("comment_composer");
+	if (commentTextArea.value.trim().length === 0) {
+		window.alert("Comment cannot be empty");
+	} else {
+		const commentsList = document.getElementById("post_comments");
+		const comment = document.createElement("li");
+		comment.setAttribute("class", "comment");
+		comment.innerText = commentTextArea.value;
+		commentsList.prepend(comment);
+	}
+};
